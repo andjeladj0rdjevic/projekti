4228d93d226976999ba89875fd014c0f418859e6
bojanafilipovic <bojanaf25@gmail.com>
2026-02-10 16:48:54 +0100

    Promenjen utils.h

--- Projekat1_PIS_RG/utils.h
+++ Projekat1_PIS_RG/utils.h
@@ -1,5 +1,7 @@
 #ifndef UTILS_H
 #define UTILS_H
+#include <GL/freeglut.h>
+#include <vector>
 
 void drawGrid();
 
@@ -8,5 +10,23 @@ void drawForces();
 
 void addSupport(float mx, float my, float angle);
 void drawSupports();
+
+struct Node {
+    float x, y;
+
+struct Element {
+    int nodeStart;
+    int nodeEnd;
+
+extern Node nodes[100];
+extern int nodeCount;
+extern Element elements[200];
+extern int elementCount;
+
+void drawTruss();
+void handleMouse(int button, int state, int x, int y);
+int findClosestNode(float x, float y);
+void addNode(float x, float y);
+void addElement(int startIdx, int endIdx);
 
 #endif
